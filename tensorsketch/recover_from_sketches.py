f8cd03fd5e78aacb25465fc0474b1d93b00f264c
Yiming <sunstat@stanford.edu>
2019-10-29 07:26:13 -0700

    Keep rewrite the package

--- tensorsketch/recover_from_sketches.py
+++ tensorsketch/recover_from_sketches.py
@@ -1,8 +1,8 @@
 #######################
-#                     #
-#  Yiming Sun         #
-#  2019               #
-#                     #
+#                     *
+#  Yiming Sun         *
+#  2019               *
+#                     *
 #######################
 
 """
@@ -21,6 +21,29 @@ from tensorly.decomposition import tucker
 from .sketch import fetch_arm_sketch, fetch_core_sketch
 from .util import square_tensor_gen, eval_rerr, st_hosvd
 from scipy.sparse.linalg import svds
+
+
+
+def check_sketch_size_valid(arm_sketches, core_sketch, ranks, typ='i'):
+    """
+    :param arm_sketches:  arm_sketch (I_n k_n)
+    :param core_sketch:  (s_1, ..., s_n)
+    :param ranks: (r_1, ..., r_n)
+    :param typ: 'i' means in memory, 'f' fix rank
+    :return:
+    """
+    ss = core_sketch.shape
+    kk = [arm_sketch.shape[1] for arm_sketch in arm_sketches]
+    flag = []
+    for i, s in enumerate(ss):
+        if typ == 'i'
+            flag.append((s > ranks[i]) & (s>kk[i]) & (kk[i]>ranks[i]))
+        elif typ == 'f':
+            flag.append((s > ranks[i]) & (kk[i] > ranks[i]))
+        else:
+            raise Exception('typ is not supported')
+    assert all(flag), "Correct the size for sketches you input"
+
 
 
 class SketchTwoPassRecover(object):
@@ -88,7 +111,12 @@ class SketchTwoPassRecover(object):
 
 
 
+
+
+
+
 class SketchOnePassRecover(object):
+
 
     def __init__(self, X, arm_sketches, core_sketch, phis, ranks):
         tl.set_backend('numpy')
@@ -101,11 +129,14 @@ class SketchOnePassRecover(object):
         self.ranks = ranks
 
 
+
     def in_memory_fix_rank_recover(self, mode='st_hosvd'):
         '''
         Obtain the recovered tensor X_hat, core and arm tensor given the sketches
         using the one pass sketching algorithm 
         '''
+        check_sketch_size_valid(self.arm_sketches, self.core_sketch, self.ranks, typ='i')
+
         Qs = []
         for arm_sketch in self.arm_sketches:
             Q, _ = np.linalg.qr(arm_sketch)
@@ -127,6 +158,12 @@ class SketchOnePassRecover(object):
         X_hat = tl.tucker_to_tensor((self.core_tensor, self.arms))
 
         return X_hat, self.arms, self.core_tensor
+
+    def fix_rank_recover(self):
+        check_sketch_size_valid(self.arm_sketches, self.core_sketch, self.ranks, typ='f')
+
+
+
 
 
 def test_st_hosvd():
