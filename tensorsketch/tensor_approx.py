b74578ba181f8605f8abfe9ef25a847c242eeb21
Yiming <sunstat@stanford.edu>
2019-11-03 15:53:47 -0800

    Fisinih low rank approximation for tensor_approx

--- tensorsketch/tensor_approx.py
+++ tensorsketch/tensor_approx.py
@@ -75,7 +75,7 @@ class TensorApprox(object):
         return X_hat, (tucker_core, tucker_factors), (core_sketch, arm_sketches), rerr, (sketch_time, recover_time)
 
 
-    def low_rank_tensor_approx_sketch(self, sketch, method):
+    def low_rank_tensor_approx(self, method):
         """
         :param sketch: list of two elements, first is arm_sketches (list)
         the second is the core sketch
@@ -84,30 +84,8 @@ class TensorApprox(object):
         """
         # Construct the approximation directly from sketch
         start_time = time.time()
-        arm_sketches, core_sketch = sketch
 
-        if method == "hooi":
-            # in this method, no sketch needed, so set as 'empty'
-            core_sketch = np.zeros(1)
-            arm_sketches = [[] for _ in np.arange(len(self.X.shape))]
-            core, tucker_factors = tucker(self.X, self.ranks, init='svd')
-            X_hat = tl.tucker_to_tensor(core, tucker_factors)
-            running_time = time.time() - start_time
-            sketch_time = -1
-            recover_time = running_time
-
-        elif method == "st_hosvd":
-            # in this method, no sketch needed, so set as 'empty'
-            core_sketch = np.zeros(1)
-            arm_sketches = [[] for _ in np.arange(len(self.X.shape))]
-            core, tucker_factors = st_hosvd(self.X, self.ranks)
-            X_hat = tl.tucker_to_tensor(core, tucker_factors)
-            running_time = time.time() - start_time
-            sketch_time = -1
-            recover_time = running_time
-
-
-        elif method == "two_pass":
+        if method == "two_pass":
             arm_sketches, _ = fetch_arm_sketch(self.X, self.ks, **self.random_setting)
             # core_sketch = fetch_core_sketch(self.X, self.ss, typ=self.rm_typ)
             core_sketch = None
@@ -115,21 +93,62 @@ class TensorApprox(object):
             start_time = time.time()
             sketch_two_pass = SketchTwoPassRecover(self.X, arm_sketches, self.ranks)
             X_hat, tucker_factors, tucker_core = \
-                sketch_two_pass.in_memory_fix_rank_recover(mode='st_hosvd')
+                sketch_two_pass.low_rank_recover()
 
             recover_time = time.time() - start_time
 
         elif method == "one_pass":
             sketch_time = time.time() - start_time
             start_time = time.time()
-            sketch_one_pass = SketchOnePassRecover(arm_sketches, core_sketch, phis, self.ranks)
-            X_hat, _, _ = sketch_one_pass.recover()
+            arm_sketches, _ = fetch_arm_sketch(self.X, self.ks, **self.random_setting)
+            core_sketch, phis = fetch_core_sketch(self.X, self.ss, **self.random_setting)
+            sketch_one_pass = SketchOnePassRecover(core_sketch, arm_sketches, phis, self.ranks)
+            X_hat, tucker_factors, tucker_core = sketch_one_pass.low_rank_recover()
+            X_hat, _, _ = sketch_one_pass.low_rank_recover()
             recover_time = time.time() - start_time
         else:
             raise Exception("please use either of the three methods: "
                             "st_hosvd, hooi, twopass, onepass")
         # Compute the the relative error when the true low rank tensor is unknown. 
         # Refer to simulation.py in case when the true low rank tensor is given. 
+        rerr = eval_rerr(self.X, X_hat, self.X)
+        return X_hat, core_sketch, arm_sketches, rerr, (sketch_time, recover_time)
+
+
+    def fix_rank_tensor_approx(self, method):
+        """
+        :param sketch: list of two elements, first is arm_sketches (list)
+        the second is the core sketch
+        :param method: name for the optimization method
+        :return:
+        """
+        # Construct the approximation directly from sketch
+        start_time = time.time()
+
+        if method == "two_pass":
+            arm_sketches, _ = fetch_arm_sketch(self.X, self.ks, **self.random_setting)
+            # core_sketch = fetch_core_sketch(self.X, self.ss, typ=self.rm_typ)
+            core_sketch = None
+            sketch_time = time.time() - start_time
+            start_time = time.time()
+            sketch_two_pass = SketchTwoPassRecover(self.X, arm_sketches, self.ranks)
+            X_hat, tucker_factors, tucker_core = \
+                sketch_two_pass.low_rank_recover()
+
+            recover_time = time.time() - start_time
+
+        elif method == "one_pass":
+            sketch_time = time.time() - start_time
+            start_time = time.time()
+            sketch_one_pass = SketchOnePassRecover(core_sketch, arm_sketches, phis, self.ranks)
+            X_hat, tucker_factors, tucker_core = sketch_one_pass.low_rank_recover()
+            X_hat, _, _ = sketch_one_pass.low_rank_recover()
+            recover_time = time.time() - start_time
+        else:
+            raise Exception("please use either of the three methods: "
+                            "st_hosvd, hooi, twopass, onepass")
+        # Compute the the relative error when the true low rank tensor is unknown.
+        # Refer to simulation.py in case when the true low rank tensor is given.
         rerr = eval_rerr(self.X, X_hat, self.X)
         return X_hat, core_sketch, arm_sketches, rerr, (sketch_time, recover_time)
 
@@ -152,16 +171,31 @@ if __name__ == "__main__":
                               noise_level, seed=1)
     tapprox1 = TensorApprox(X, ranks, ks, ss)
 
-    print("now testing st_hosvd")
-    _, _, _, rerr, _ = tapprox1.in_memory_fix_rank_tensor_approx("st_hosvd")
-    print(rerr)
+    def test_in_memory_fix_rank():
+        print("now testing st_hosvd")
+        _, _, _, rerr, _ = tapprox1.in_memory_fix_rank_tensor_approx("st_hosvd")
+        print(rerr)
 
-    print("now testing two pass")
-    _, _, _, rerr, _ = tapprox1.in_memory_fix_rank_tensor_approx("two_pass")
-    print(rerr)
+        print("now testing two pass")
+        _, _, _, rerr, _ = tapprox1.in_memory_fix_rank_tensor_approx("two_pass")
+        print(rerr)
 
-    _, _, _, rerr, _ = tapprox1.in_memory_fix_rank_tensor_approx("one_pass")
-    print(rerr)
+        print("now testing one pass")
+        _, _, _, rerr, _ = tapprox1.in_memory_fix_rank_tensor_approx("one_pass")
+        print(rerr)
 
-    # Test it for data with unequal side length
+
+    def test_low_rank():
+
+        print("now testing two pass")
+        _, _, _, rerr, _ = tapprox1.low_rank_tensor_approx("two_pass")
+        print(rerr)
+
+        print("now testing one pass")
+        _, _, _, rerr, _ = tapprox1.low_rank_tensor_approx("one_pass")
+        print(rerr)
+
+    # test_in_memory_fix_rank()
+    test_low_rank()
+
 
